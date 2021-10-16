b8434fc65d4e79d924e00aa3d4e5a6ba356e778a
Surojit <81943886+thesurojit-das@users.noreply.github.com>
2021-10-16 11:03:47 +0530

    Update Bubblesort.py

--- Bubblesort.py
+++ Bubblesort.py
@@ -1,41 +1,25 @@
-#include <bits/stdc++.h>
-using namespace std;
- 
-void swap(int *xp, int *yp)
-{
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
- 
-// A function to implement bubble sort
-void bubbleSort(int arr[], int n)
-{
-    int i, j;
-    for (i = 0; i < n-1; i++)    
-     
-    // Last i elements are already in place
-    for (j = 0; j < n-i-1; j++)
-        if (arr[j] > arr[j+1])
-            swap(&arr[j], &arr[j+1]);
-}
- 
-/* Function to print an array */
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i = 0; i < size; i++)
-        cout << arr[i] << " ";
-    cout << endl;
-}
- 
-// Driver code
-int main()
-{
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    bubbleSort(arr, n);
-    cout<<"Sorted array: \n";
-    printArray(arr, n);
-    return 0;
-}
+# Python program for implementation of Bubble Sort
+
+def bubbleSort(arr):
+	n = len(arr)
+
+	# Traverse through all array elements
+	for i in range(n):
+
+		# Last i elements are already in place
+		for j in range(0, n-i-1):
+
+			# traverse the array from 0 to n-i-1
+			# Swap if the element found is greater
+			# than the next element
+			if arr[j] > arr[j+1] :
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+
+# Driver code to test above
+arr = [64, 34, 25, 12, 22, 11, 90]
+
+bubbleSort(arr)
+
+print ("Sorted array is:")
+for i in range(len(arr)):
+	print ("%d" %arr[i]),
